b8fb8a605ada19d682a24d0fb87e5d1da15d39e3
Vijay S Kumar <vijay.kumar@tifin.com>
2022-09-03 20:43:52 +0530

    Adding util functions to generate and verify access tokens using JWT.

---
+++ server/utils/config/jwt.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import { AccessTokenPayload } from "../../types";
+
+const TOKEN_EXPIRE_DURATION = 60; //6 hrs in secs.
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
+const generateAccessToken = (payload: AccessTokenPayload) =>
+  jwt.sign({ payload }, ACCESS_TOKEN_SECRET, {
+    expiresIn: TOKEN_EXPIRE_DURATION,
+  });
+
+const verifyAccessToken = async (token: string): Promise<number> => {
+  let statusCode = 200;
+  await jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      statusCode = 403;
+    }
+    if (!user) statusCode = 401;
+  });
+  return statusCode;
+};
+
+export { generateAccessToken, verifyAccessToken };
