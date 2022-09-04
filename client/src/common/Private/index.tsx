141006ba4f4ce9937decec0fba0c9f0bf1ba9b40
Vijay S Kumar <vijay.kumar@tifin.com>
2022-09-04 09:02:14 +0530

    Configuring Private Routes.

--- client/src/common/Private/index.tsx
+++ client/src/common/Private/index.tsx
@@ -1,14 +1,13 @@
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
+import { getAccessToken } from "utils";
 
 export const PrivateRoute = () => {
   const navigate = useNavigate();
-  //   const authUser = { id: "some", token: "some" };
-  const authUser = null;
-  console.log("Auth User ", authUser);
 
   useEffect(() => {
-    if (!authUser) {
+    if (!getAccessToken()) {
+      console.log({ tokn: getAccessToken() });
       navigate("/login");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
