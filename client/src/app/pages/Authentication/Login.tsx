eff6d18e1c3e19f13b4b254461cc7386226aea85
Vijay S Kumar <vijay.kumar@tifin.com>
2022-09-04 09:03:38 +0530

    Feature: Sign in an user.

--- client/src/app/pages/Authentication/Login.tsx
+++ client/src/app/pages/Authentication/Login.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { AxiosError } from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "services/api/auth";
+import { APIResponse } from "types";
+import { extractErrorInfo, setAccessToken } from "utils";
+import { notifyError, notifySuccess } from "utils/notifications";
+import { AuthInfo } from "./types";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginUser = async () => {
+    try {
+      const {
+        data: { message, data: responseData },
+      }: { data: APIResponse } = await login({ email, password });
+
+      const { accessToken } = responseData as AuthInfo;
+      setAccessToken(accessToken);
+      notifySuccess(message);
+      navigate("/dashboard");
+    } catch (error) {
+      notifyError(extractErrorInfo(error as AxiosError));
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Login</h1>
+      <br />
+      <p>Add Validation for this</p>
+      <input
+        type="text"
+        placeholder="Enter your email.."
+        onChange={({ target: { value } }) => setEmail(value)}
+      />
+      <br />
+      <p>Add Validation for this</p>
+      <input
+        type="text" //TODO: Convert this to password.
+        placeholder="Enter your password"
+        onChange={({ target: { value } }) => setPassword(value)}
+      />
+      <br />
+      <br />
+
+      <button onClick={() => loginUser()}>Let in</button>
+      <br />
+      <br />
+
+      <p>
+        Dropped in for the first time? <Link to={"/register"}>Sign Up</Link>{" "}
+      </p>
+    </div>
+  );
 };
 
 export default Login;
