8df8d8077227d1e2b4fb8d8bb649c3d5b25342ed
Vijay S Kumar <vijay.kumar@tifin.com>
2022-09-04 08:52:03 +0530

    Exception handling for Sign up function.

--- server/api/auth/signUp.ts
+++ server/api/auth/signUp.ts
@@ -4,7 +4,7 @@ import { User } from "./types";
 import userModel from "../../model/User";
 import { APIResponse } from "../../types";
 import { GENERIC_ERROR } from "../../constants";
-import { getSuccessResponse } from "../../utils";
+import { getErrorResponse, getSuccessResponse } from "../../utils";
 
 const SALT_ROUNDS = 10;
 
@@ -19,26 +19,42 @@ const signUp = async ({
 
   try {
     bcrypt.hash(password, SALT_ROUNDS, async (err, hash) => {
-      if (err) return response.status(401).send(GENERIC_ERROR);
+      if (err) return response.status(500).send(GENERIC_ERROR);
 
       if (hash) {
-        await userModel.create({
-          email,
-          password: hash,
-          username,
-        });
-        response.status(200).send(
-          getSuccessResponse({
-            data: { email, password, username } as User,
-            message:
-              "Signed Up Successfully. You can use these credentials to Login!",
+        userModel
+          .create({
+            email,
+            password: hash,
+            username,
           })
-        );
+          .then((user) =>
+            response.status(200).send(
+              getSuccessResponse({
+                data: { email, username } as User,
+                message:
+                  "Signed Up Successfully. You can use these credentials to Login!",
+              })
+            )
+          )
+          .catch((err) => {
+            // 11000 -> MongoServerError code for duplicate records. For sign-up only EMAIL is the duplicate.
+            if (err.code === 11000)
+              return response
+                .status(409)
+                .json(
+                  getErrorResponse(
+                    "An account exists with this Email. Please use a different email instead."
+                  )
+                );
+            // Return a generic response if something else is wrong.
+            response.status(500).json(GENERIC_ERROR);
+          });
       }
     });
   } catch (error) {
     console.error(`Failed to sign up the user with email ${email} `, error);
-    response.status(401).json(GENERIC_ERROR);
+    response.status(500).json(GENERIC_ERROR);
   }
 };
 
