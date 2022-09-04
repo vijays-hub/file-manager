10ce2a3073905d18403eb253099e71bd547d56a5
Vijay S Kumar <vijay.kumar@tifin.com>
2022-09-04 09:03:03 +0530

    Mock Dashboard.

--- client/src/app/pages/Dashboard/index.tsx
+++ client/src/app/pages/Dashboard/index.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Logout from "../Authentication/Logout";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <ul>
+        <li>Setup Redux / react-query </li>
+        <li>
+          Get user info by email (should be available with the AuthInfo. Store
+          it in state after login)
+        </li>
+        <li>Rest of the stuff</li>
+      </ul>
+      <button>
+        <Logout />
+      </button>
+    </div>
+  );
 };
 
 export default Dashboard;
