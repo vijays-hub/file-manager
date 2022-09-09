379307480160463f7cf32ff2544d88f464b7026d
Vijay S Kumar <vijay.kumar@tifin.com>
2022-09-09 18:30:51 +0530

    Adding an API to check user's authenticity.

--- client/src/services/api/user/index.ts
+++ client/src/services/api/user/index.ts
@@ -3,4 +3,6 @@ import { apiInstance } from "config/axios";
 const getUserByEmail = async (email: string) =>
   await apiInstance().get("/users/user-by-email", { params: { email } });
 
-export { getUserByEmail };
+const getAuthenticity = async () => await apiInstance().get("/users/auth");
+
+export { getUserByEmail, getAuthenticity };
