a5fe7214742d7e16e998346c070acbb14e52a32c
Vijay S Kumar <vijay.kumar@tifin.com>
2022-09-09 18:29:58 +0530

    Adding auth route to check user's authenticity.

--- server/api/user/index.ts
+++ server/api/user/index.ts
@@ -1,8 +1,15 @@
 import express, { Request, Response } from "express";
 import { authGateway } from "../../middleware/authGateway";
+import { getAccessTokenFromHeaders } from "../../utils/config/jwt";
+import { getAuthUserResponse } from "./constants";
 import { fetchUserByEmail } from "./fetchUserByEmail";
 
 const router = express.Router();
+
+router.get("/auth", authGateway, (request: Request, response: Response) => {
+  const token = getAccessTokenFromHeaders(request);
+  response.status(200).send(getAuthUserResponse(token));
+});
 
 router.get(
   "/user-by-email",
